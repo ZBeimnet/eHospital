5392b5f5df446aaf9b248c28a9785179f4a1d458
Beimnet.Z <zbeimnet@gmail.com>
2018-12-10 23:03:12 +0300

    HomeController and home.html(unfinished)

---
+++ src/main/java/atech/webdivision/demo/HomeController.java
@@ -0,0 +1,13 @@
+package atech.webdivision.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
