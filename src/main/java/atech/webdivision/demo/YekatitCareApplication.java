dbde62f3bbe2539e9de11c9210d8354a173fa720
Beimnet.Z <zbeimnet@gmail.com>
2018-12-10 20:12:49 +0300

    Initial Commit

---
+++ src/main/java/atech/webdivision/demo/YekatitCareApplication.java
@@ -0,0 +1,12 @@
+package atech.webdivision.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YekatitCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YekatitCareApplication.class, args);
+    }
+}
---
+++ src/test/java/atech/webdivision/demo/YekatitCareApplicationTests.java
@@ -0,0 +1,16 @@
+package atech.webdivision.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YekatitCareApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
